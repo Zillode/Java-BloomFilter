71bff948c931cd6fd66c633792cff52f361d5e59
Lode Hoste <zillode@zillode.be>
2015-09-01 22:21:16 +0200

    Clarify license based on LGPL header and specific details in README

--- src/com/skjegstad/utils/BloomFilter.java
+++ src/com/skjegstad/utils/BloomFilter.java
@@ -11,6 +11,12 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If you want to avoid adding another library to your project, you may copy
+ * this code directly into your project if you leave the LGPL-comment in place
+ * and reference the java-bloomfilter web page:
+ * https://github.com/magnuss/java-bloomfilter
+ *
  */
 
 package com.skjegstad.utils;
